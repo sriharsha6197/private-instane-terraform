5efc0436daa41a3d571a413372f1b14cb794c6c1
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-30 00:56:01 +0530

    adding routes to the internet routing table

--- resources/private.tf
+++ resources/private.tf
@@ -27,6 +27,10 @@ resource "aws_internet_gateway" "igw2" {
 
 resource "aws_route_table" "internet_route_table" {
   vpc_id = aws_vpc.vpc_2.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw2.id
+  }
 }
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc_2.id
