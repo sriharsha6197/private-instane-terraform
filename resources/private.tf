bdb797ab39820b531a237ece950f397ee3c853ae
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-29 23:59:24 +0530

    private vpc configuration setup

---
+++ resources/private.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc" "vpc_2" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc_2"
+  }
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.vpc_2.id
+  cidr_block = "10.0.0.0/17"
+  tags = {
+    Name = "subnet1"
+  }
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.vpc_2.id
+  cidr_block = "10.0.128.0/17"
+  tags = {
+    Name = "subnet2"
+  }
+}
+resource "aws_internet_gateway" "igw2" {
+  vpc_id = aws_vpc.vpc_2.id
+  tags = {
+    Name = "igw2"
+  }
+}
+
+resource "aws_route_table" "internet_route_table" {
+  vpc_id = aws_vpc.vpc_2.id
+  route {
+    cidr_block = "10.0.0.0/17"
+    gateway_id = aws_internet_gateway.igw2.id
+  }
+}
