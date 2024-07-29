cea676c54b43c6d3504277399ae0ee456cf5c847
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-30 00:27:18 +0530

    renaming of sec group

--- resources/private.tf
+++ resources/private.tf
@@ -42,16 +42,16 @@ resource "aws_security_group" "allow-all-sg" {
   vpc_id      = aws_vpc.vpc_2.id
 
   tags = {
-    Name = "sg-allow-all"
+    Name = "allow-all-sg"
   }
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.sg-allow-all.id
+  security_group_id = aws_security_group.allow-all-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol = "-1"
 }
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.sg-allow-all.id
+  security_group_id = aws_security_group.allow-all-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
