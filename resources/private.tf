ef82c1c8be9298718c8741ebfed89f0b16a4d278
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-30 00:47:14 +0530

    nat gw creation

--- resources/private.tf
+++ resources/private.tf
@@ -58,3 +58,15 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 resource "aws_eip" "eip" {
   domain   = "vpc"
 }
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.subnet1.id
+  connectivity_type = "public"
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw2]
+}
