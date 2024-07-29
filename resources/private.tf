6ab45f6b4b82b65168c28b267f68c20ff2851c59
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-30 00:57:50 +0530

    adding route to the private routing table

--- resources/private.tf
+++ resources/private.tf
@@ -34,6 +34,10 @@ resource "aws_route_table" "internet_route_table" {
 }
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc_2.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
 }
 resource "aws_route_table_association" "internet" {
   subnet_id      = aws_subnet.subnet1.id
