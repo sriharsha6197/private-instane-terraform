9bbce77686aa9da5e49690d51ce70bc9a782138c
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-30 01:20:58 +0530

    sg retrieval

--- resources/data.tf
+++ resources/data.tf
@@ -1,6 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  owners           = 973714476881
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = [973714476881]
 }
 data "aws_subnet" "subnet2" {
   id = var.subnet2
@@ -8,9 +9,9 @@ data "aws_subnet" "subnet2" {
 variable "subnet2" {
   default = "subnet-099626c70b91fce83"
 }
-data "aws_security_groups" "ig_sg" {
-  id = var.security_group_id
-}
 variable "security_group_id" {
   default = "sg-09577d329cf16367a"
+}
+data "aws_security_groups" "ig_sg" {
+  id = var.security_group_id
 }
