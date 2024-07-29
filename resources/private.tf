91a18f7628df9e970c798e508cbc5e8b83155a11
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-30 00:25:01 +0530

    route table and sec group

--- resources/private.tf
+++ resources/private.tf
@@ -31,3 +31,27 @@ resource "aws_route_table" "internet_route_table" {
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc_2.id
 }
+resource "aws_route_table_association" "internet" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.internet_route_table.id
+}
+
+resource "aws_security_group" "sg-allow-all" {
+  name        = "sg-allow-all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc_2.id
+
+  tags = {
+    Name = "sg-allow-all"
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.sg-allow-all.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg-allow-all.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
